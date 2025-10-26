e2a56662a10a69394ddc2854ced40ea6648ddf16
srinbeen <srinivass.2003@gmail.com>
2025-10-26 16:45:03 -0700

    flake.nix added

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "env for vinyl collection application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # Packages for development
+        packages = with pkgs; [
+		clang
+		cmake
+
+		sqlite
+        ];
+      };
+    };
+}
